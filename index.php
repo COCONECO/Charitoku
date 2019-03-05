6b738b39a20c1cfef655c8ed6d8c32d0161e72de
fwgc1945 <fwgc1945@gmail.com>
2019-03-05 10:23:30 +0900

    新規追加

---
+++ index.php
@@ -0,0 +1,268 @@
+<!DOCTYPE html>
+<html lang="ja">
+
+<head>
+    <meta charset="utf-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=Edge">
+    <title>知っとく走っとく徳島サイクリングロード</title>
+    <meta name="description" content="">
+    <meta name="keywords" content="">
+
+    <meta name="viewport" content="width=device-width,initial-scale=1.0">
+    <meta name="format-detection" content="telephone=no">
+
+    <!-- Fonts -->
+    <link href="https://fonts.googleapis.com/css?family=Noto+Sans+JP:500,700" rel="stylesheet">
+    <link href="https://fonts.googleapis.com/css?family=Sawarabi+Mincho" rel="stylesheet">
+
+    <!-- stylesheets -->
+    <link rel="stylesheet" href="css/style.css" media="all">
+    <link rel="stylesheet" href="css/top.css" media="all">
+</head>
+
+<body>
+
+    <header id="commonHeader">
+        <div id="gnavLogo">
+            <a href="#">
+                <h1>logo<img src="#"></h1>
+            </a>
+        </div>
+        <nav id="gnav">
+            <ul>
+                <li><a href="route-list.html">
+                        <p class="navTitle">Route</p>
+                        <p class="navSubTitle">ルート一覧</p>
+                    </a></li>
+                <li><a href="introduction.html">
+                        <p class="navTitle">Info</p>
+                        <p class="navSubTitle">自転車の心得</p>
+                    </a></li>
+                <li><a href="blog-list.html">
+                        <p class="navTitle">Blog</p>
+                        <p class="navSubTitle">ブログ</p>
+                    </a></li>
+                <li><a href="link.html">
+                        <p class="navTitle">Link</p>
+                        <p class="navSubTitle">リンク</p>
+                    </a></li>
+            </ul>
+        </nav>
+        <div class="scroll">
+            <a href="">
+                <div class="arrow1"></div>
+                <P>scroll</P>
+            </a>
+        </div>
+        </nav>
+    </header>
+
+    <div class="tIMG">
+        <img src="images/top/IMG_7466.JPG" class="topImg imgChange"
+            style="width: 1536px; height: auto; left: calc(50vw - 700px);">
+    </div>
+
+    <!-- インフォメーション -->
+    <!-- 新着blog記事 -->
+
+
+
+    <div class="test">
+        <div class="textBlog">
+            <div class="block coutseInfo">
+                <h2>新着<br>記事</h2>
+            </div>
+
+            <div class="topBlog">
+                <div id="blogList">
+
+
+                    <div class="profile_card">
+                        <div class="card_photo">
+                            <div class="card_photo_img"> <img src="images/top/20170704230336.jpg" class="boxbox">
+                            </div>
+                        </div>
+                        <div class="profile_bio">
+                            <h3>2019年3月4日</h3>
+                            <br>
+                            <p><a href="#">記事タイトル</a></p>
+                        </div>
+                    </div>
+
+
+
+
+                    <div class="profile_card">
+
+                        <div class="card_photo">
+                            <div class="card_photo_img"> <img src="images/top/20170704230336.jpg" class="boxbox">
+                            </div>
+                        </div>
+                        <div class="profile_bio">
+                            <h3>2019年3月4日</h3>
+                            <br>
+                            <p class="title-archive"><a href="#">記事タイトル</a></p>
+                        </div>
+                    </div>
+
+
+                </div>
+            </div>
+            <div style="text-align: right"><a href="#" class="more">→more</a></div>
+        </div>
+
+
+        <div class="text">
+            <div class="block">
+                <h2>イベント<br>情報</h2>
+            </div>
+            <div class="rss">
+                <ul>
+                    <li>「渡船(約5分)で行く早春の鳴門満喫観光ライド」開催のお知らせ（2019年2月14日</li>
+                    <li>日本でいちばん楽しい自転車教室がやってくる！ウィーラースクール開催のお知らせ（2019年2月1日）</li>
+                    <li>「TOKUSHIMAサイクルフェスタvol.2」開催のお知らせ（2019年1月29日)</li>
+                    <li>『TOKUSHIMAサイクルフェスタ ランニングバイクレース』開催のお知らせ（2019年1月28日）</li>
+                    <li>２月のミニガイド開催のお知らせ（2019年1月23日）</li>
+                    <li>１月のミニガイドツーリング開催のお知らせ（2018年12月14日）</li>
+                    <li>健康サイクリング部の実施について（2018年12月10日）</li>
+                    <li>１１月のミニガイドツーリング開催のお知らせ（2018年10月22日）</li>
+                    <li>１０月のミニガイドツーリング開催のお知らせ（2018年10月5日）</li>
+                    <li>「サイクルトレイン阿波池田」開催のお知らせ（2018年9月19日）</li>
+                </ul>
+            </div>
+            <div style="text-align: right"><a href="#" class="more">→more</a></div>
+        </div>
+    </div>
+
+
+    <!-- おすすめコース -->
+
+
+
+
+    <div>
+        <div class="topBox">
+            <h2 class="topH2">おすすめ<br>コース</h2>
+        </div>
+    </div>
+
+    <div class=topCourse>
+        <ul id="itemList">
+            <li class="itemBox grid5Pc grid10">
+                <a href="#">
+                    <img class="mainImg">
+                    <img class="subImg">
+                    <img class="subImg">
+                    <img class="subImg">
+                    <div class="left">
+                        <h3 class="routeTitle">初級ルート１</h3>
+                        <p class="description">説明文</p>
+                        <p class="description">説明文</p>
+                        <dl>
+                            <dt>走行距離</dt>
+                            <dd>10km</dd>
+                            <dt>走行時間</dt>
+                            <dd>約1時間</dd>
+                            <dt>消費カロリー</dt>
+                            <dd>1000kcal</dd>
+                        </dl>
+                    </div>
+                    <div class="right">
+                        <div class="level beginner">初級</div>
+                        <p class="detail">詳細へ</p>
+                    </div>
+                </a>
+            </li>
+            <li class="itemBox grid5Pc grid10">
+                <a href="#">
+                    <img class="mainImg">
+                    <img class="subImg">
+                    <img class="subImg">
+                    <img class="subImg">
+                    <div class="left">
+                        <h3 class="routeTitle">初級ルート１</h3>
+                        <p class="description">説明文</p>
+                        <p class="description">説明文</p>
+                        <dl>
+                            <dt>走行距離</dt>
+                            <dd>10km</dd>
+                            <dt>走行時間</dt>
+                            <dd>約1時間</dd>
+                            <dt>消費カロリー</dt>
+                            <dd>1000kcal</dd>
+                        </dl>
+                    </div>
+                    <div class="right">
+                        <div class="level beginner">初級</div>
+                        <p class="detail">詳細へ</p>
+                    </div>
+                </a>
+            </li>
+            <li class="itemBox grid5Pc grid10">
+                <a href="#">
+                    <img class="mainImg">
+                    <img class="subImg">
+                    <img class="subImg">
+                    <img class="subImg">
+                    <div class="left">
+                        <h3 class="routeTitle">初級ルート１</h3>
+                        <p class="description">説明文</p>
+                        <p class="description">説明文</p>
+                        <dl>
+                            <dt>走行距離</dt>
+                            <dd>10km</dd>
+                            <dt>走行時間</dt>
+                            <dd>約1時間</dd>
+                            <dt>消費カロリー</dt>
+                            <dd>1000kcal</dd>
+                        </dl>
+                    </div>
+                    <div class="right">
+                        <div class="level beginner">初級</div>
+                        <p class="detail">詳細へ</p>
+                    </div>
+                </a>
+            </li>
+            <li class="itemBox grid5Pc grid10">
+                <a href="#">
+                    <img class="mainImg">
+                    <img class="subImg">
+                    <img class="subImg">
+                    <img class="subImg">
+                    <div class="left">
+                        <h3 class="routeTitle">初級ルート１</h3>
+                        <p class="description">説明文</p>
+                        <p class="description">説明文</p>
+                        <dl>
+                            <dt>走行距離</dt>
+                            <dd>10km</dd>
+                            <dt>走行時間</dt>
+                            <dd>約1時間</dd>
+                            <dt>消費カロリー</dt>
+                            <dd>1000kcal</dd>
+                        </dl>
+                    </div>
+                    <div class="right">
+                        <div class="level beginner">初級</div>
+                        <p class="detail">詳細へ</p>
+                    </div>
+                </a>
+            </li>
+        </ul>
+    </div>
+    <div><a href="#" class="more routemore">→more</a></div>
+
+    <footer>
+        <div class="back">
+            <ul class="banner">
+                <li><a href="">バナー</a></li>
+                <li><a href="">バナー</a></li>
+                <li><a href="">バナー</a></li>
+            </ul>
+        </div>
+    </footer>
+
+
+</body>
+
+</html>
