0f4e902e818d824b2bbcf1466afc5eb7fa8b3138
hiorufmitakiguchi <takki-2@amber.plala.or.jp>
2019-03-07 21:43:56 +0900

    blog詳細ページにアニメーションログを追加しました。

--- wordpress/single.php
+++ wordpress/single.php
@@ -1,4 +1,3 @@
-
 <!DOCTYPE html>
 <html>
 
@@ -15,45 +14,63 @@
 
 get_header();
 ?>
+
 <body>
     <header id="commonHeader">
-        <div id= "gnavLogo">
-            <a href="http://localhost/charitoku/"><h1>logo<img src=""></h1></a>
+        <div id="gnavLogo">
+            <a href="http://localhost/charitoku/index.php">
+                <h1>知っとく走っとく徳島サイクリングロード<div class="logoImages">
+                        <div>
+                </h1>
+            </a>
         </div>
         <nav id="gnav">
             <ul>
-            <li><a href="http://localhost/charitoku/route-list.php?level=all"><p class="navTitle">Route</p><p class="navSubTitle">ルート一覧</p></a></li>
-                <li><a href="http://localhost/charitoku/introduction.php"><p class="navTitle">Info</p><p class="navSubTitle">自転車の心得</p></a></li>
-                <li><a href="http://localhost/charitoku/blog/wordpress/"><p class="navTitle">Blog</p><p class="navSubTitle">ブログ</p></a></li>
-                <li><a href="http://localhost/charitoku/link.php"><p class="navTitle">Link</p><p class="navSubTitle">リンク</p></a></li>
+                <li><a href="http://localhost/charitoku/route-list.php?level=all">
+                        <p class="navTitle">Route</p>
+                        <p class="navSubTitle">ルート一覧</p>
+                    </a></li>
+                <li><a href="http://localhost/charitoku/introduction.php">
+                        <p class="navTitle">Info</p>
+                        <p class="navSubTitle">自転車の心得</p>
+                    </a></li>
+                <li><a href="http://localhost/charitoku/blog/wordpress/">
+                        <p class="navTitle">Blog</p>
+                        <p class="navSubTitle">ブログ</p>
+                    </a></li>
+                <li><a href="http://localhost/charitoku/link.php">
+                        <p class="navTitle">Link</p>
+                        <p class="navSubTitle">リンク</p>
+                    </a></li>
             </ul>
         </nav>
     </header>
 
     <div class="tutumi">
-    <h2 class="midashi"><span>
-        <?php the_title();?>
-        </span>
-    </h2>
-    <div class="shosai">
-      <p class="date"><?php echo get_the_date(); ?></p>
-      <p class="category"><?php the_category();?></p>
+        <h2 class="midashi"><span>
+                <?php the_title(); ?>
+            </span>
+        </h2>
+        <div class="shosai">
+            <p class="date"><?php echo get_the_date(); ?></p>
+            <p class="category"><?php the_category(); ?></p>
+        </div>
+
+
+        <?php
+ /* Start the Loop */
+        while (have_posts()) : the_post();
+            the_content();
+        endwhile; // End of the loop.
+        ?>
+        <p class="back"><a href="http://localhost/charitoku/blog/wordpress/">→記事一覧に戻る</a></p>
+    </div>
+    </div>
+    <div class="sideBar detailSideBar">
+        <?php get_sidebar(); ?>
     </div>
 
-
-<?php
-/* Start the Loop */
-while (have_posts()): the_post();
-    the_content();
-endwhile; // End of the loop.
-?>
-<p class="back"><a href="http://localhost/charitoku/blog/wordpress/">→記事一覧に戻る</a></p>
-  </div>
-  </div>
-<div class="sideBar detailSideBar">
-    <?php get_sidebar();?>
-</div>
-
-    <?php get_footer();?>
+    <?php get_footer(); ?>
 </body>
-</html>
+
+</html> 
