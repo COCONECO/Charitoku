b79dbe91c3b36d5ad37723bbb536a23501805247
Fujimoto_ryo <pokapoka.moka@gmail.com>
2019-03-12 15:27:17 +0900

    フォントが少し違うかったので修正

--- blog/wordpress/header.php
+++ blog/wordpress/header.php
@@ -20,8 +20,7 @@
     <title>記事一覧</title>
     <meta name="viewport" content="width=device-width, initial-scale=1">
     <!-- Fonts -->
-    <link href="https://fonts.googleapis.com/css?family=Noto+Sans+JP:500,700" rel="stylesheet">
-    <link href="https://fonts.googleapis.com/css?family=Sawarabi+Mincho" rel="stylesheet">
+    <link href="https://fonts.googleapis.com/css?family=Noto+Sans+JP|Sawarabi+Mincho" rel="stylesheet">
 
       <!-- icon -->
       <link href="https://use.fontawesome.com/releases/v5.6.1/css/all.css" rel="stylesheet">
@@ -36,5 +35,5 @@
     <link rel="stylesheet" id="dashicons-css" href="http://localhost/charitoku/blog/wordpress/wp-content/themes/twentynineteen/css/blog-details.css" type="text/css" media="all"> -->
 
     <!-- <link rel="stylesheet" id="dashicons-css" href="http://localhost/charitoku/blog/wordpress/wp-content/themes/twentynineteen/kijistyle.css" type="text/css" media="all"> -->
-    <link href="https://fonts.googleapis.com/css?family=Noto+Sans+JP:400,700|Sawarabi+Mincho&amp;subset=japanese" rel="stylesheet">
+
 </head>
