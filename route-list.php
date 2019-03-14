0183683c3b738ca2b4c5d2dd74beef2c9e835b7b
tanaka_koyo <makiarenren@gmail.com>
2019-03-14 18:12:50 +0900

    スマートフォン（デベロッパーツール）の表示で正しくメディアクエリが機能するようにheaderにmetaデータを追加

--- route-list.php
+++ route-list.php
@@ -2,6 +2,7 @@
 <html>
 
 <head>
+    <meta name="viewport" content="width=device-width,initial-scale=1.0">
     <link rel="stylesheet" href="css/style.css">
     <link rel="stylesheet" href="css/route-list.css">
     <link href="https://fonts.googleapis.com/css?family=Noto+Sans+JP|Sawarabi+Mincho" rel="stylesheet">
